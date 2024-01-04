7eecf4131d3f76dd68a19847b7ee9694c6ce362f
DavisDabols <d.dabols@gmail.com>
2024-01-04 16:36:38 +0200

    Pievienota raksta atvēršana no rezultātu loga

--- Pages/results.py
+++ Pages/results.py
@@ -1,13 +1,24 @@
 import tkinter as tk
 from tkinter import ttk
+import webbrowser as wb
+
+def onDoubleClick(event):
+    SearchTree = event.widget
+    article = SearchTree.identify('item',event.x,event.y)
+    link = SearchTree.item(article)['values'][2]
+    wb.open_new_tab(link)
 
 def searchResults(results):
+
     sresult = tk.Toplevel()
     sresult.title("Meklēšanas rezultāti")
     sresult.geometry("850x500")
 
     DescriptionLabel = tk.Label(sresult, text="Meklēšanas rezultāti", font=('Verdana', 18))
     DescriptionLabel.pack(padx=10, pady=10)
+
+    HelpLabel = tk.Label(sresult, text="Nospiediet dubultklikšķi, lai atvērtu rakstu", font=('Verdana', 10))
+    HelpLabel.pack(padx=10, pady=10)
 
     # Rāmis tabulai, kura parāda visus meklēšanas rezultātus
     ResultFrame = tk.Frame(sresult)
@@ -24,6 +35,7 @@ def searchResults(results):
             print(article)
             SearchTree.insert('', tk.END, values=(result, results[result][article], article))
     SearchTree.grid(row=0, column=0, sticky="nsew")
+    SearchTree.bind('<Double-1>', onDoubleClick)
     # Pievieno ritjoslu rāmim
     scrollbar = ttk.Scrollbar(ResultFrame, orient=tk.VERTICAL, command=SearchTree.yview)
     SearchTree.configure(yscroll=scrollbar.set)
